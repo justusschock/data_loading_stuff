f61484d639aab17e8f335d3553d0ecc95b9bef87
Justus Schock <justus.schock@rwth-aachen.de>
2019-06-25 09:26:55 +0200

    remove deprecated stuff

--- data_loading/nii.py
+++ data_loading/nii.py
@@ -4,7 +4,6 @@ import os
 from abc import abstractmethod
 
 import SimpleITK as sitk
-import numpy as np
 
 from delira.utils.decorators import make_deprecated
 
@@ -25,49 +24,6 @@ def load_nii(path):
         numpy array containing the loaded data
     """
     return sitk.GetArrayFromImage(sitk.ReadImage(path))
-
-
-@make_deprecated('LoadSample')
-def load_sample_nii(files, label_load_cls):
-    """
-    Load sample from multiple ITK files
-
-    .. deprecated-removed:: 0.3.4 0.4
-
-    Parameters
-    ----------
-    files : dict with keys `img` and `label`
-        filenames of nifti files and label file
-    label_load_cls : class
-        function to be used for label parsing
-
-    Returns
-    -------
-    dict
-        sample: dict with keys `data` and `label` containing images and label
-
-    Raises
-    ------
-    AssertionError
-        if `img.max()` is greater than 511 or smaller than 1
-
-    """
-    img_list = []
-    for f in files['img']:
-        img = sitk.GetArrayFromImage(sitk.ReadImage(f))
-        img = img.astype(np.float32)
-        assert img.max() <= 511
-        assert img.max() > 1
-        img = img / 511
-        img_list.append(img)
-    label_gen = label_load_cls(files['label'])
-    label = label_gen.get_labels()
-    sample = {"data": np.stack(img_list), "label": label}
-    if 'mask' in list(files.keys()):
-        mask = sitk.GetArrayFromImage(sitk.ReadImage(files['mask']))
-        mask = mask.astype(np.int64)
-        sample['mask'] = mask
-    return sample
 
 
 class BaseLabelGenerator(object):
